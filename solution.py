debefe6d77e378c944a3992c4df2a00ff82c3290
ryzhval <130060842+ryzhval@users.noreply.github.com>
2023-04-07 11:21:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 371649437 # Ваш chat ID, не меняйте название переменной
+chat_id = 997785064 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    a = np.mean(x)/98
+    a = (np.mean(x)-23+np.exp(1))/10 
     return a # Ваш ответ
